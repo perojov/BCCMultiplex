ee4a43141da477d912bcf6b41e71b75e73a6e9b9
Petar Jovanovski <89967659+perojov@users.noreply.github.com>
2024-09-20 00:05:39 +0200

    Delete Applications directory

--- Applications/LazegaVisualization.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-for sim in range(1, 8):
-    alpha_trace = np.load('Lazega/Sim' + str(sim) + 'alphatraceK4.npy')
-    plt.plot(alpha_trace.T)
-    print(np.mean(alpha_trace, axis=1))
-    plt.show()
-#pi_trace = np.load('Lazega/Sim1pitraceK3.npy')
